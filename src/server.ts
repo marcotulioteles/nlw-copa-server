59d1268a002623da2fd77dd422097c36aa8a7e2a
Marco Túlio Teles <m.reis@pimenta.group>
2022-11-03 23:34:35 -0300

    feat: create migrations for all tables in database

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import { z } from 'zod'
+import shortUniqueId from 'short-unique-id'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient({
@@ -9,7 +11,7 @@ const prisma = new PrismaClient({
 async function bootstrap() {
   const fastify = Fastify({
     logger: true
-  })
+  }) 
 
   await fastify.register(cors, {
     origin: true
@@ -19,6 +21,38 @@ async function bootstrap() {
     const count = await prisma.pool.count()
 
     return { count }
+  })
+  
+  fastify.get('/users/count', async () => {
+    const count = await prisma.user.count()
+
+    return { count }
+  })
+  
+  fastify.get('/guesses/count', async () => {
+    const count = await prisma.guess.count()
+
+    return { count }
+  })
+  
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string()
+    })
+
+    const { title } = createPoolBody.parse(request.body)
+
+    const generate = new shortUniqueId({ length: 6 })
+    const code = String(generate()).toUpperCase()
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code
+      }
+    })
+
+    return reply.status(201).send({ code })
   })
 
   await fastify.listen({ port: 3333, host: '0.0.0.0' })
